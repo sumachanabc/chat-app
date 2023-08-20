0a60c7a6a89500f8d4a70fd4de487b6c11f420a3
sumachanabc <bike.cbrider@gmail.com>
2023-08-21 01:10:31 +0900

    Revert "画面遷移の実装"
    This reverts commit a44cce62766613c0264bd68af77bff538a7bf5bb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "rooms#index"
+  get 'messages/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: "messages#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
